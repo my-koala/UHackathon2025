8e44f2f75b1fdacd3a96d064d4eaa76a48deb750
unknown <mykoaladev@gmail.com>
2025-05-17 12:13:42 -0700

    Added main game loop

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,34 +1,70 @@
 @tool
 extends Node
 
+# TODO: static noise animation
+
 @export
-var microgame_scenes: Array[MicroGame] = []
-var _microgame_index: int = 0
+var micro_games: Array[MicroGame] = []
+var _micro_game_current: MicroGame = null
 
 @export
 var lives: int = 5
 var _lives: int = 5
 
 @export
-var run_microgames: bool = false
-var _microgame_count: int = 0
+var run_micro_games: bool = false
+var _micro_game_iteration: int = 0
 
 @export
-var minigame_time_scale_curve: Curve = Curve.new()
+var micro_game_time_scale_curve: Curve = Curve.new()
+
+@onready
+var _animation_player: AnimationPlayer = $animation_player as AnimationPlayer
 
 
 func _ready() -> void:
 	if Engine.is_editor_hint():
 		return
 	
-	_lives = lives
+	# play animations?
+	# await for prompts?
+	loop_start()
 	
-	microgame_scenes
 
 func loop_start() -> void:
 	_lives = lives
-	_microgame_index = 0
-	microgame_scenes.shuffle()
+	_micro_game_iteration = 0
+	micro_games.shuffle()
+	
+	_micro_game_current = micro_games[micro_games.size() % _micro_game_iteration]
+	_micro_game_current.on_complete.connect(_on_current_microgame_completed)
+	# TODO: await for animation?
+	
+	_animation_player.play(&"microgame_enter")
+	await _animation_player.animation_finished
+	
+	_micro_game_current.start(micro_game_time_scale_curve.sample(float(_micro_game_iteration)))
+
+func _on_current_microgame_completed(success: bool) -> void:
+	if !success:
+		print("not successful, decrement lives")
+		_lives -= 1
+		# TODO: play life lost animation
+	else:
+		print("successful")
+		# play some sort of success animation
+	
+	_animation_player.play(&"microgame_exit")
+	await _animation_player.animation_finished
+	
+	_micro_game_iteration += 1
+	loop_update()
+
+func loop_update() -> void:
+	if _lives <= 0:
+		print("game over")
+		
+		# end the game, probably some animation
 	
 	
 
