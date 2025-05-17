7d3f1928cee973c236fe4e59b9968529ef404ca7
Caeden117 <caeden.s@outlook.com>
2025-05-17 12:17:43 -0700

    Update microgame.gd

--- scripts/microgame/microgame.gd
+++ scripts/microgame/microgame.gd
@@ -6,8 +6,7 @@ signal on_complete(success: bool)
 @export
 var time_duration: float = 7.5
 
-var time_scale: float = 1
-
+var _time_scale: float = 1
 var _time_left: float = 7.5
 var _running: bool = false
 
@@ -15,9 +14,12 @@ var _running: bool = false
 func _ready() -> void:
 	start(1)
 
+func get_time_left() -> float:
+	return _time_left
+
 ## Starts the microgame
 func start(time_scale: float) -> void:
-	self.time_scale = time_scale
+	_time_scale = time_scale
 	_time_left = time_duration
 	_running = true
 
@@ -34,7 +36,7 @@ func _physics_process(delta: float) -> void:
 	if !_running:
 		return
 	
-	_time_left -= time_scale * delta
+	_time_left -= _time_scale * delta
 	
 	if (_time_left <= 0):
 		complete(false)
