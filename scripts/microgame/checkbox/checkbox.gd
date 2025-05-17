5f54332a74838ffc507bd2bceb6d83327440016d
legoandmars <legoandmars@gmail.com>
2025-05-17 14:51:25 -0700

    Add checkbox dvd logo behaviour based on difficulty scaling

--- scripts/microgame/checkbox/checkbox.gd
+++ scripts/microgame/checkbox/checkbox.gd
@@ -2,8 +2,9 @@ class_name CheckboxMicroGame
 extends MicroGame
 
 @onready
-var checkbox: CheckBox = $check_box
+var checkbox: CheckBoxDvd = $check_box
 
 func start(time_scale: float, difficulty_scale: float) -> void:
 	checkbox.set_pressed_no_signal(false)
+	checkbox.initialize(difficulty_scale)
 	super(time_scale, difficulty_scale)
