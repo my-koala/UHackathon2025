cdf37bb9bea570221024dc889971ef6553187803
unknown <mykoaladev@gmail.com>
2025-05-17 11:49:35 -0700

    Adjusted main scene tree

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,6 +2,14 @@
 extends Node
 
 @export
+var microgame_scenes: Array[MicroGame] = []
+var _microgame_index: int = 0
+
+@export
+var lives: int = 5
+var _lives: int = 5
+
+@export
 var run_microgames: bool = false
 var _microgame_count: int = 0
 
@@ -12,6 +20,15 @@ var minigame_time_scale_curve: Curve = Curve.new()
 func _ready() -> void:
 	if Engine.is_editor_hint():
 		return
+	
+	_lives = lives
+	
+	microgame_scenes
+
+func loop_start() -> void:
+	_lives = lives
+	_microgame_index = 0
+	microgame_scenes.shuffle()
 	
 	
 
